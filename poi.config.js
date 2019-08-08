0ffa4a70ddac6885edc2ddc8296d34a3ffe2a45c
Paul Bastowski <pbastowski@gmail.com>
2019-08-08 13:09:14 +0200

    Don't compile .html files with svelte, thus only supporting the new .svelte components in version 3. This is required to prevent svelte from trying to compile the public/index.html file and failing miserably.

--- poi.config.js
+++ poi.config.js
@@ -15,7 +15,7 @@ module.exports = {
         module: {
             rules: [
                 {
-                    test: /\.(html|svelte)$/,
+                    test: /\.svelte$/,
                     exclude: /node_modules/,
                     use: {
                         loader: 'svelte-loader',
