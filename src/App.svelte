ed0a597eeec3381c0ae5742ae1d4e9d7a74eddbd
pbastowski <pbastowski@users.noreply.github.com>
2019-08-10 21:30:19 +0200

    Clean up

--- src/App.svelte
+++ src/App.svelte
@@ -3,20 +3,9 @@
     let text = 'testing'
 </script>
 
-<style>
-    :global(pre) {
-        background: yellow;
-    }
-</style>
-
-<h1>Hello Svelte 42</h1>
-<p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea id impedit iste
-    laborum obcaecati officiis quam reiciendis sapiente soluta voluptate!
-    Cupiditate doloremque dolores earum ex id molestiae, nihil rerum unde!
-</p>
+<h1>Hello Svelte</h1>
 
 <svelte:options tag="my-thing" />
 
-<Input bind:value={text} style="color: red;">Label</Input>
-{text}
+<Input bind:value={text}>Label</Input>
+<pre>text: { text }</pre>
