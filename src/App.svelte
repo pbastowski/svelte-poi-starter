e2e7f8b0d1d426afe06e2395b7de4594e4d113f4
Paul Bastowski <pbastowski@gmail.com>
2019-08-12 23:10:29 +0200

    Add example showing how to dispatch a Svelte synthetic event 'svelte-event', when compiling to a web component target. Remove unnecessary console.log from function emit. Add comments explaining how to dispatch and then handle Svelte events as well as JavaScript custom events.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script>
+    const dispatch = require('svelte').createEventDispatcher()
     import Input from './Input.svelte'
 
     let text = 'testing'
 
     function emit(el, name, detail) {
-        console.log('Sending CLICK event')
         el.dispatchEvent(
             new Event(name, {
                 detail,
@@ -19,16 +19,27 @@
 
 <svelte:options tag="my-thing" accessors={true} />
 
+<!--
+    Events dispatched with Svelte using createEventDispatcher() can not
+    be handled with normal javascript event handling. That is
+    `element.addEventListener('svelte-event', handler)` and
+    `<element onsvelte-event="handler()">` will NOT work.
+    Instead you have to use `element.$on('svelte-event', handler)`.
+    See index.html for an example.
+
+    Events dispatched with element.dispatchEvent(new Event('custom'))
+    can be only be handled in JavaScript using
+    `element.addEventListener('custom', handler)`.
+    See index.html for an example.
+-->
+
 <Input
-    on:click={e => emit(e.target, 'custom')}
+    on:click={e => {
+        dispatch('svelte-event')
+        emit(e.target, 'custom')
+    }}
     bind:value={text}>
     Label
 </Input>
 
 <pre>text: {text}</pre>
-
-<!--
-    on:click={ev => {
-        domEvent(ev.target, 'custom')
-    }}
--->
